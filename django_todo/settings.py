7802a5f158b13bdcdbd9228918bb5f81d1ed9be2
Artem Bryzh <aab@AMBM2.local>
2024-07-31 17:39:52 +0100

    Add todo template

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from decouple import config
 
+# Ensure that env.py is loaded
+import env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
