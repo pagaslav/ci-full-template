61ee46db5d9e39192c39bbe5345bea8f4166fe69
Artem Bryzh <aab@AMBM2.local>
2024-08-16 15:02:09 +0100

    Add Allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['ckz8779-django-todo-app.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ckz8779-django-todo-app-56867531a50b.herokuapp.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
